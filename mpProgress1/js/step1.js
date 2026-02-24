ef30f3a8bb76f97d08df2ea9af59be3578094820
charlottenovoa <cnovoa@sfsu.edu>
2026-02-24 11:28:46 -0800

    commit 2

---
+++ mpProgress1/js/step1.js
@@ -0,0 +1,17 @@
+
+function setup() {
+    createCanvas(600, 300);
+    noStroke();
+  }
+  
+  function draw() {
+    fill("#df9fb3");
+    rect(0, 0, 200, 300);
+  
+    fill("#faf9eb");
+    rect(150, 0, 200, 300);
+  
+    fill("#4e372e");
+    rect(300, 0, 200, 300);
+  
+  }
