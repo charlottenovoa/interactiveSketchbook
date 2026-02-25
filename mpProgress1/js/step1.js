1a8b1af0b45b4dc221c89d3c605ae1166e426df5
charlottenovoa <cnovoa@sfsu.edu>
2026-02-24 16:34:16 -0800

    half

--- mpProgress1/js/step1.js
+++ mpProgress1/js/step1.js
@@ -1,10 +1,12 @@
+let bite=0;
+
 
 function setup() {
     createCanvas(600, 300);
-    noStroke();
   }
   
   function draw() {
+    background(200);
     fill("#df9fb3");
     rect(0, 0, 200, 300);
   
@@ -14,11 +16,26 @@ function setup() {
     fill("#4e372e");
     rect(300, 0, 200, 300);
 
-    // fill(150);
-    // ellipse(0,0,200);
+    fill(150);
+    ellipse(0,0,200);
+
+    drawSpoon(mouseX, mouseY, 100);
+    
+    // push();
+    // ellipse(155, 150, 20); 
+    // rect(150, 155, 10, 50);
+    // pop();
+    
   
   }
 
+  function drawSpoon() {
+    ellipse(155, 150, 20); 
+    rect(150, 155, 10, 50);
+
+  }
+  
+
   function mousePressed() {
-    // bite will appear
+    bite += 1;
   }
