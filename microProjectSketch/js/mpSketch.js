800ef42ce599f91f9e0b96f9e8ef53570e4b9b42
charlottenovoa <cnovoa@sfsu.edu>
2026-03-17 13:13:58 -0700

    more

--- microProjectSketch/js/mpSketch.js
+++ microProjectSketch/js/mpSketch.js
@@ -1,74 +1,64 @@
-let images = [];
-let currentIndex = 0;
 let steps=0;
-let maxSteps=7;
+let maxSteps=10;
+let pic;
+let img;
 
 function preload() {
-    images[0] = loadImage("../microProjectSketch/img/neopolitan.png");
-    images[1] = loadImage("../microProjectSketch/img/neopolitan1.png");
-    images[2] = loadImage("../microProjectSketch/img/neopolitan2.png");
-    images[3] = loadImage("../microProjectSketch/img/neopolitan3.png");
-    images[4] = loadImage("../microProjectSketch/img/neopolitan4.png");
-    images[5] = loadImage("../microProjectSketch/img/neopolitan5.png");
-    images[6] = loadImage("../microProjectSketch/img/neopolitan6.png");
-    images[7] = loadImage("../microProjectSketch/img/neopolitan7.png");
+    pic = loadImage("../microProjectSketch/img/neo.JPEG");
+    // img = loadImage("../microProjectSketch/img/overlay.PNG");
+
 
 
 }
 
 function setup() {
-    createCanvas(600,300);
+    createCanvas(1000,600);
     textSize(16);
+
+    imageMode(CENTER);
+
+    
+
 
 }
 
 function draw() {
     background(0);
 
-    let sectionHeight = height / maxSteps;
+    image(pic, width/2, height/2);
+    image(img, width/2, height/2);
 
-    let img = images[currentIndex];
-    image(img, 0, 0);
-
-    fill("#4e372e");
-    textAlign(CENTER, TOP);
-    textSize(20);
-    text("press mouse to bite!", width/2, 20);
-
-    noStroke();
-  for (let i =0; i<steps; i++) {
-    fill("#487540");
-    rect(5, i * sectionHeight, 20, sectionHeight);
-  }
+//     fill("#4e372e");
+//     textAlign(CENTER, TOP);
+//     textSize(20);
+//     text("press mouse to bite!", width/2, 20);
+//   }
 
 }
 
 function mousePressed() {
-        currentIndex++;
-        if(currentIndex >= images.length) {
-            currentIndex = 0;
-        }
-
-        if (steps < maxSteps) {
-            steps += 1;
-          }
+    if (steps < maxSteps) {
+        steps += 1;
+      }
+      
+    fill("#848484");
+    noStroke();
+    ellipse(mouseX, mouseY, 150, 150); 
 }
 
-function keyPressed() {
-    steps = 0;
-}
+if (steps = 10) {
+    fill("#8968a2");
+    rect(400, 300, width/2, height/2);
+    
+    
 
-if (steps = maxSteps) {
-    fill("#ef8291");
-    rect(width/2, height/2, 400, 100);
-
-    fill("0");
+    fill("#4e372e");
     textAlign(CENTER);
     textSize(20);
-    text("FULL", width/2, height/2);
-
-
+    text("I'm Full!", width/2, height/2);
+//   }
 }
+
 
 
   
