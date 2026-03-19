9ba1707f8c20e8295b4d71d1a2424ece23cef0ac
charlottenovoa <cnovoa@sfsu.edu>
2026-03-19 13:24:11 -0700

    its good

--- microProjectSketch/js/mpSketch.js
+++ microProjectSketch/js/mpSketch.js
@@ -1,63 +1,52 @@
-let steps=0;
-let maxSteps=10;
 let pic;
 let img;
+let bites = [];
+let maxBites;
 
 function preload() {
-    pic = loadImage("../microProjectSketch/img/neo.JPEG");
-    // img = loadImage("../microProjectSketch/img/overlay.PNG");
-
-
-
+    pic = loadImage("../microProjectSketch/img/neo.png");
+    img = loadImage("../microProjectSketch/img/popUp.png"); 
 }
 
 function setup() {
-    createCanvas(1000,600);
-    textSize(16);
-
+    createCanvas(1000, 600);
     imageMode(CENTER);
+    textSize(20);
 
-    
-
-
+    maxBites = int(random(10, 30));
 }
 
 function draw() {
     background(0);
 
     image(pic, width/2, height/2);
-    image(img, width/2, height/2);
 
-//     fill("#4e372e");
-//     textAlign(CENTER, TOP);
-//     textSize(20);
-//     text("press mouse to bite!", width/2, 20);
-//   }
 
+    noStroke();
+    fill("#f2f2ed");
+    for (let b of bites) {
+        ellipse(b.x, b.y, 200, 200);
+    }
+
+    if (bites.length >= maxBites) {
+        image(img, width/2, height/2);
+    }
+
+    fill("#4e372e");
+    textAlign(CENTER, TOP);
+    text("press mouse to bite!", width/2, 40);
 }
 
 function mousePressed() {
-    if (steps < maxSteps) {
-        steps += 1;
-      }
-      
-    fill("#848484");
-    noStroke();
-    ellipse(mouseX, mouseY, 150, 150); 
+    if (bites.length < maxBites) {
+        bites.push({ x: mouseX, y: mouseY });
+    }
 }
 
-if (steps = 10) {
-    fill("#8968a2");
-    rect(400, 300, width/2, height/2);
-    
-    
-
-    fill("#4e372e");
-    textAlign(CENTER);
-    textSize(20);
-    text("I'm Full!", width/2, height/2);
-//   }
+function keyPressed() {
+    bites = [];
 }
+
 
 
 
